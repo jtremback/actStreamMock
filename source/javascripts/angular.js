7379cb793c5457a3621a40f33255b4236e8a9a6c
Jehan Tremback <jehan.tremback@gmail.com>
2012-07-24 08:52:19 -0700

    first commit, added all files

---
+++ source/javascripts/angular.js
@@ -0,0 +1,21 @@
+function annoList($scope) {
+  $scope.annotations= [
+    {
+      name:"RLCoolstein", 
+      time:"about 3 hours ago", 
+      page:"Courageous class battlecruiser - Wikipedia, the free encyclopedia",
+      link:"http://en.wikipedia.org/wiki/Courageous_class_battlecruiser", 
+      info:"Shared by: JengaMaster",
+      text: "I have two separate editions of Brooks, with two difference sets of pagination. In neither edition, on page 170, is there anything to actually confirm, Data from a 15-foot (4.6 m) rangefinder in the armoured hood was input into a Mk IV* Dreyer Fire Control Table located in the Transmitting Station (TS) where it was converted into range and deflection data for use by the guns. I would suggest a closer reading of the pages involved.",
+      excerpt:"The Courageous class comprised three battlecruisers built for the Royal Navy during World War I. Nominally designed to support Admiral of the Fleet Lord John Fisher's Baltic Project, which was intended to land troops on the German Baltic Coast, ships of this class were fast but very lightly armoured with only a few heavy guns. To maximize their speed, the Courageous-class battlecruisers were the first capital ships of the Royal Navy to use geared steam turbines and small-tube boilers. "
+    },
+    {
+      name:"MugOfJurassic", 
+      time:"about 4 hours ago", 
+      page:"http://en.wikipedia.org/wiki/Passive_voice", 
+      info:"Shared by: Arnold" ,
+      text: 'I know of no reliable sources suggesting that Romance languages use the passive voice more often or more conspicuously than Germanic languages, let alone that Romance languages use passive voice more often than active voice. On the contrary, the only scholarly sources I could find saying anything near this suggest just the opposite. For example, Jisa, Reilly, Verhoeven, Baruch, and Rosado (2002) suggest that "When a language has productive alternative rhetorical options to remove the agent from topic position, it tends to use passive voice less often. In short, this claim is unverified and needs to cite a source or be removed from the article."',
+      excerpt:"Passive voice is a grammatical voice common in many of the world's languages. Passive is used in a clause whose subject expresses the theme or patient of the main verb. That is, the subject undergoes an action or has its state changed. A sentence whose theme is marked as grammatical subject is called a passive sentence. In contrast, a sentence in which the subject has the agent role is called an active sentence, and its verb is expressed in active voice. Many languages have both an active and a passive voice; this allows for greater flexibility in sentence construction, as either the semantic agent or patient may take the syntactic role of subject."
+    }
+  ];
+}
